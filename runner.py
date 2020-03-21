829e380fb5d19f33e030ad774e4487b4c2aa7c9f
Liu Zechu <liuzechu2013@gmail.com>
2020-03-21 12:17:31 +0800

    fix bugs in runner.py

--- runner.py
+++ runner.py
@@ -14,11 +14,11 @@ Translates to executing:
     python CS3243_P2_Sudoku_13.py public_tests_p2_sudoku/input1.txt output1.txt
 """
 
-# argv[0] represents input number of test case
-if len(sys.argv) != 1:
+# argv[1] represents input number of test case
+if len(sys.argv) != 2:
     print("Wrong number of arguments! Try: ./runner.py <test input num>")
 
-input_num = sys.argv[0]
+input_num = sys.argv[1]
 file_name = "CS3243_P2_Sudoku_13.py"
 
 input_path = "public_tests_p2_sudoku/input{n}.txt".format(n=input_num)
@@ -34,23 +34,33 @@ start = time.time()
 
 # run program
 print("Running {filename} on input{n}.txt".format(filename=file_name, n=input_num))
-subprocess.call(["python", filename, input_path, output_file])
+subprocess.call(["python", file_name, input_path, output_file])
 end = time.time()
 print("Completed.\nDuration: {0} seconds".format(round(end-start, 2)))
 
 # check answer for correctness
-lines = f.readlines()
-f.close()
+answer_file = open("public_tests_p2_sudoku/output{n}.txt".format(n=input_num),"r")
+result = []
+answer = []
+print("Your output is:")
+for line in f:
+    print line,
+    result.append(line)
 
-answer_file = open("file2.txt","r")
+for line in answer_file:
+    answer.append(line)
+
 is_different = False
-for line1 in f:
-    for line2 in answer_file:
-        if line1 == line2:
-            print("SAME\n")
-        else:
-            print(line1 + line2)
-            is_different = True
+
+for index in range(0, len(result)):
+    if result[index] == answer[index]:
+        print("line {n} is correct".format(n=index))
+    else:
+        print("line {n} is different".format(n=index))    
+        print "result: " + str(result[index]),
+        print "expected: " + str(answer[index]),
+        is_different = True
+
 if not is_different:
     print("The solution is correct!")
 
