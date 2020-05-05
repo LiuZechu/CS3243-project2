85a9d772e9dd265d573686270b3d21d1c09969b3
Larry Law <larrylaw97@gmail.com>
2020-05-05 16:36:41 +0800

    Update results.txt

--- sudoku/runall.py
+++ sudoku/runall.py
@@ -4,11 +4,9 @@ import subprocess
 
 """
 HOW IT WORKS:
-    ./runner.py <test input num>
-For example, typing:
-    ./runner.py 1
+    ./runall.py
 Translates to executing:
-    python CS3243_P2_Sudoku_13.py public_tests_p2_sudoku/input1.txt output1.txt
+    python CS3243_P2_Sudoku_13.py input{i}.txt output{i}.txt # for i \in [1, 5]
 """
 
 sudoku = "CS3243_P2_Sudoku_13.py"
@@ -17,4 +15,4 @@ output = "output"
 extension = ".txt"
 
 for i in range(1, 5):
-    subprocess.call(["python2", sudoku, input + str(i) + extension, output + str(i) + extension])
+    subprocess.call(["python", sudoku, input + str(i) + extension, output + str(i) + extension])
