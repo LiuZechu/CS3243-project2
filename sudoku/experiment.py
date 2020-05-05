1c76b684fb8f314f0d235a7adc9a418208251cd1
Liu Zechu <liuzechu2013@gmail.com>
2020-05-06 00:10:07 +0800

    modify experiment

--- sudoku/experiment.py
+++ sudoku/experiment.py
@@ -11,9 +11,8 @@ from CS3243_P2_Sudoku_13 import Sudoku
 To run the experiments, run ./experiment.py in the terminal.
 """
 
-
 ###################### SEEDING RANDOM GENERATOR ###############
-random.seed(30)
+random.seed(20)
 
 ################### TESTCASES ######################
 # read test cases from csv file
@@ -45,7 +44,7 @@ def read_test_cases_from_file(file_name):
 
         # TODO: change this hard coded part for exp 2
         if file_name == "experiment_inputs_2.csv":
-            number_of_empty_cells = 50
+            number_of_empty_cells = 60
 
         for k in range(number_of_empty_cells):
             row = randrange(9)
@@ -74,37 +73,16 @@ def run_and_generate_stats(puzzle):
     return duration
 
 ############### EXPERIMENT 1: TIME AGAINST SOLUTION DEPTH ###############
-puzzles = read_test_cases_from_file("experiment_inputs_1.csv")
+# puzzles = read_test_cases_from_file("experiment_inputs_1.csv")
 
-# counter = 1
-# for puzzle in puzzles:
-#     duration = run_and_generate_stats(puzzle)
-#     print("solution depth {} takes ".format(counter) + str(duration) + " seconds")
-#     counter += 1
-
-###### Write into a CSV file ######
-output_file = "experiment_1_output.csv"
-
-# clean previous outputs
-if os.path.isfile(output_file):
-    os.remove(output_file)
-
-delimiter = ","
-table_headings = "Numer of Empty Cells,Time Taken\n"
-with open(output_file, 'w+') as f:
-    f.write(table_headings)
-    counter = 1
-    for puzzle in puzzles:
-        duration = run_and_generate_stats(puzzle)
-        f.write(str(counter) + delimiter + str(duration) + "\n")
-        print("solution depth {} takes ".format(counter) + str(duration) + " seconds")
-        counter += 1
-
-############### EXPERIMENT 2: HISTOGRAM ###############
-# puzzles = read_test_cases_from_file("experiment_inputs_2.csv")
+# # counter = 1
+# # for puzzle in puzzles:
+# #     duration = run_and_generate_stats(puzzle)
+# #     print("solution depth {} takes ".format(counter) + str(duration) + " seconds")
+# #     counter += 1
 
 # ###### Write into a CSV file ######
-# output_file = "experiment_2_output.csv"
+# output_file = "experiment_1_output.csv"
 
 # # clean previous outputs
 # if os.path.isfile(output_file):
@@ -113,6 +91,7 @@ with open(output_file, 'w+') as f:
 # delimiter = ","
 # table_headings = "Numer of Empty Cells,Time Taken\n"
 # with open(output_file, 'w+') as f:
+#     f.write(table_headings)
 #     counter = 1
 #     for puzzle in puzzles:
 #         duration = run_and_generate_stats(puzzle)
@@ -120,25 +99,22 @@ with open(output_file, 'w+') as f:
 #         print("solution depth {} takes ".format(counter) + str(duration) + " seconds")
 #         counter += 1
 
+############### EXPERIMENT 2: HISTOGRAM ###############
+puzzles = read_test_cases_from_file("experiment_inputs_2.csv")
 
+###### Write into a CSV file ######
+output_file = "experiment_2_output.csv"
 
+# clean previous outputs
+if os.path.isfile(output_file):
+    os.remove(output_file)
 
-
-
-
-
-
-
-# ############### EXPERIMENT 1: TIME AGAINST SOLUTION DEPTH ###############
-
-# """
-# Run the algorithm under experiment using randomly generated sudoku puzzles with
-# 0 - N empty cells. Generate a csv file with time against solution depth (number of empty cells)
-# """
-
-# print("\nThis script takes around 2-3 mins to complete.")
-# print("\n===============================")
-# print("BEGINNING EXPERIMENT 1:")
-# print("===============================\n")
-
-# print("Experiment 1 completed.\n")
+delimiter = ","
+table_headings = "Numer of Empty Cells,Time Taken\n"
+with open(output_file, 'w+') as f:
+    counter = 1
+    for puzzle in puzzles:
+        duration = run_and_generate_stats(puzzle)
+        f.write(str(counter) + delimiter + str(duration) + "\n")
+        print("solution depth {} takes ".format(counter) + str(duration) + " seconds")
+        counter += 1
